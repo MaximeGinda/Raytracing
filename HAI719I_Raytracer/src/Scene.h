b328b073e561f62794df374e875132b9cc8f92a4
MaximeGinda <maximeginda@outlook.com>
2022-12-07 15:40:23 +0100

    Update Scene.h

--- HAI719I_Raytracer/src/Scene.h
+++ HAI719I_Raytracer/src/Scene.h
@@ -405,7 +405,8 @@ public:
         }
     }
 
-    void setup_single_mesh(){
+    void setup_single_mesh()
+    {
         meshes.clear();
         spheres.clear();
         squares.clear();
@@ -422,15 +423,84 @@ public:
             light.isInCamSpace = false;
         }
         {
-            meshes.resize(meshes.size() + 1);
+            meshes.resize(spheres.size() + 1);
             Mesh &m = meshes[meshes.size() - 1];
             m.loadOFF("data/avion_n.off");
             m.centerAndScaleToUnit();
+            /* m.scale(Vec3(2., 2., 2.));
+            m.translate(Vec3(0., 0., 0.)); */
             m.material.type = Material_Diffuse_Blinn_Phong;
             m.material.diffuse_material = Vec3(1., 0., 0.);
             m.material.specular_material = Vec3(1., 1., 1.);
             m.material.shininess = 16;
             m.build_arrays();
+
+        }
+        
+
+        { //Left Wall
+
+            squares.resize(squares.size() + 1);
+            Square &s = squares[squares.size() - 1];
+            s.setQuad(Vec3(-1., -1., 0.), Vec3(1., 0, 0.), Vec3(0., 1, 0.), 2., 2.);
+            s.scale(Vec3(2., 2., 1.));
+            s.translate(Vec3(0., 0., -2.));
+            s.rotate_y(90);
+            s.build_arrays();
+            s.material.diffuse_material = Vec3(1., 0., 0.);
+            s.material.specular_material = Vec3(1., 0., 0.);
+            s.material.shininess = 16;
+        }
+
+        { //Back Wall
+            squares.resize(squares.size() + 1);
+            Square &s = squares[squares.size() - 1];
+            s.setQuad(Vec3(-1., -1., 0.), Vec3(1., 0, 0.), Vec3(0., 1, 0.), 2., 2.);
+            s.scale(Vec3(2., 2., 1.));
+            s.translate(Vec3(0., 0., -2.));
+            s.build_arrays();
+            s.material.diffuse_material = Vec3(1., 1., 1.);
+            s.material.specular_material = Vec3(1., 1., 1.);
+            s.material.shininess = 16;
+        }
+
+        { //Right Wall
+            squares.resize(squares.size() + 1);
+            Square &s = squares[squares.size() - 1];
+            s.setQuad(Vec3(-1., -1., 0.), Vec3(1., 0, 0.), Vec3(0., 1, 0.), 2., 2.);
+            s.translate(Vec3(0., 0., -2.));
+            s.scale(Vec3(2., 2., 1.));
+            s.rotate_y(-90);
+            s.build_arrays();
+            s.material.diffuse_material = Vec3(0.0, 1.0, 0.0);
+            s.material.specular_material = Vec3(0.0, 1.0, 0.0);
+            s.material.shininess = 16;
+        }
+
+        { //Floor
+            squares.resize(squares.size() + 1);
+            Square &s = squares[squares.size() - 1];
+            s.setQuad(Vec3(-1., -1., 0.), Vec3(1., 0, 0.), Vec3(0., 1, 0.), 2., 2.);
+            s.translate(Vec3(0., 0., -2.));
+            s.scale(Vec3(2., 2., 1.));
+            s.rotate_x(-90);
+            s.build_arrays();
+            s.material.diffuse_material = Vec3(1.0, 1.0, 1.0);
+            s.material.specular_material = Vec3(1.0, 1.0, 1.0);
+            s.material.shininess = 16;
+        }
+
+        { //Ceiling
+            squares.resize(squares.size() + 1);
+            Square &s = squares[squares.size() - 1];
+            s.setQuad(Vec3(-1., -1., 0.), Vec3(1., 0, 0.), Vec3(0., 1, 0.), 2., 2.);
+            s.translate(Vec3(0., 0., -2.));
+            s.scale(Vec3(2., 2., 1.));
+            s.rotate_x(90);
+            s.build_arrays();
+            s.material.diffuse_material = Vec3(1.0, 1.0, 1.0);
+            s.material.specular_material = Vec3(1.0, 1.0, 1.0);
+            s.material.shininess = 16;
         }
     }
 
