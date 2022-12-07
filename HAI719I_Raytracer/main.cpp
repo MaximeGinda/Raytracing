c089db486500692f02fbe601be5d83b4548fc630
MaximeGinda <maximeginda@outlook.com>
2022-12-07 15:39:10 +0100

    up

--- HAI719I_Raytracer/main.cpp
+++ HAI719I_Raytracer/main.cpp
@@ -324,7 +324,7 @@ int main (int argc, char ** argv) {
     scenes[0].setup_single_sphere();
     scenes[1].setup_single_square();
     scenes[2].setup_cornell_box();
-    //scenes[3].setup_single_mesh();
+    scenes[3].setup_single_mesh();
 
     glutMainLoop ();
     return EXIT_SUCCESS;
