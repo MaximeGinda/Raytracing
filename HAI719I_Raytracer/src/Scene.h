2668f0d7106ead79834e800faec0fc3378e9a966
MaximeGinda <maximeginda@outlook.com>
2023-01-07 22:43:24 +0100

    Update Scene.h

--- HAI719I_Raytracer/src/Scene.h
+++ HAI719I_Raytracer/src/Scene.h
@@ -537,7 +537,7 @@ public:
         Vec3 color;
 
         // Si la profondeur de champs est activé
-        if(dof) color = deapthOfField(rayStart, 4,9);
+        if(dof) color = deapthOfField(rayStart, 4.9);
         else color = rayTraceRecursive(rayStart, 5, 4.9);
 
         return color;
