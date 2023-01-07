8a21efa07b2f92617490c7c8b4318b9a5b32351c
MaximeGinda <maximeginda@outlook.com>
2023-01-07 16:27:47 +0100

    Update Scene.h

--- HAI719I_Raytracer/src/Scene.h
+++ HAI719I_Raytracer/src/Scene.h
@@ -106,7 +106,7 @@ struct BoundingBox {
             4, 0, 3, 3, 7, 4
         };
 
-        glBegin(GL_LINES);
+        glBegin(GL_LINE_LOOP);
         for (unsigned int i : indices) {
             glVertex3d(vertices[3 * i], vertices[3 * i + 1], vertices[3 * i + 2]);
         }
